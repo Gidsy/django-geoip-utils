250dfb123952b94249cef7d93e6fb91862cd6941
Jannis Leidel <jannis@leidel.info>
2012-01-11 12:43:12 +0100

    Fixed the default of the GEOIP_CACHE_METHOD settings since GEOIP_MEMORY_CACHE is a class attribute of django.contrib.gis.utils.GeoIP.

--- geoip_utils/settings.py
+++ geoip_utils/settings.py
@@ -1,6 +1,6 @@
 from django.conf import settings
-
+from django.contrib.gis.utils import GeoIP
 
 REQUEST_IP_RESOLVER = getattr(settings, "GEOIP_REQUEST_IP_RESOLVER", "geoip_utils.utils.remote_addr_ip")
 
-CACHE_METHOD = getattr(settings, "GEOIP_CACHE_METHOD", "GEOIP_MEMORY_CACHE")
+CACHE_METHOD = getattr(settings, "GEOIP_CACHE_METHOD", GeoIP.GEOIP_MEMORY_CACHE)
