d68eea3a9caa6c591fc1be79aee91aac1a3bf233
Philipp Wassibauer <phil@gidsy.com>
2012-01-11 11:26:50 +0100

    fixing packaging in setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ from distutils.core import setup
 setup(
     name='django-geoip-utils',
     version='0.0.2',
-    description='GeoIp data as an app to facilitate installing. Also features management commands for updating the data.',
+    description='GeoIp data as an app to facilitate installing. Also features template filters, util functions and management commands for updating the data.',
     long_description=open('README.rst').read(),
     author='Philipp Wassibauer',
     author_email='phil@gidsy.com',
-    url='http://github.com/Gidsy/django-geoip-data',
+    url='http://github.com/Gidsy/django-geoip-utils',
     packages=[
         'geoip_utils',
+        'geoip_utils.templatetags',
     ],
     package_data={'geoip_utils': [
 		'data/*.dat'
