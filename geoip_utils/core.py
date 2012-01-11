c20d0fb3fdd069e275f2c2497c4c21cc56eeae5d
Jannis Leidel <jannis@leidel.info>
2012-01-11 12:16:21 +0100

    Fixed a bunch of tabs and trailing spaces :P

--- geoip_utils/core.py
+++ geoip_utils/core.py
@@ -7,8 +7,8 @@ def get_country(request):
         g = GeoIP(cache=CACHE_METHOD)
         return g.country(ip)
     return None
-    
-    
+
+
 def get_city(request):
     ip = get_ip_of_request(request)
     if ip:
@@ -23,28 +23,28 @@ def get_lat_lon(request):
         g = GeoIP(cache=CACHE_METHOD)
         return g.lat_lon(ip)
     return None
-    
-    
+
+
 def get_country_code(request):
     ip = get_ip_of_request(request)
     if ip:
         g = GeoIP(cache=CACHE_METHOD)
         return g.country_code(ip)
     return None
-    
-    
+
+
 def get_country_name(request):
     ip = get_ip_of_request(request)
     if ip:
         g = GeoIP(cache=CACHE_METHOD)
         return g.country_name(ip)
     return None
-    
+
 
 def where():
     f = os.path.split(__file__)[0]
     return os.path.abspath(os.path.join(f, 'data'))
-    
-    
+
+
 if __name__ == '__main__':
     print where()
