d514b8921c7ec04d287cf5658bf5c7c0d09f1197
Jannis Leidel <jannis@leidel.info>
2013-01-31 09:34:03 +0100

    Initial GeoIP backend only one per process instead on every request.

--- geoip_utils/core.py
+++ geoip_utils/core.py
@@ -6,41 +6,39 @@ except ImportError:
 from .utils import get_ip_of_request
 from .settings import CACHE_METHOD
 
+geoip = GeoIP(cache=CACHE_METHOD)
+
+
 def get_country(request):
     ip = get_ip_of_request(request)
     if ip:
-        g = GeoIP(cache=CACHE_METHOD)
-        return g.country(ip)
+        return geoip.country(ip)
     return None
 
 
 def get_city(request):
     ip = get_ip_of_request(request)
     if ip:
-        g = GeoIP(cache=CACHE_METHOD)
-        return g.city(ip)
+        return geoip.city(ip)
     return None
 
 
 def get_lat_lon(request):
     ip = get_ip_of_request(request)
     if ip:
-        g = GeoIP(cache=CACHE_METHOD)
-        return g.lat_lon(ip)
+        return geoip.lat_lon(ip)
     return None
 
 
 def get_country_code(request):
     ip = get_ip_of_request(request)
     if ip:
-        g = GeoIP(cache=CACHE_METHOD)
-        return g.country_code(ip)
+        return geoip.country_code(ip)
     return None
 
 
 def get_country_name(request):
     ip = get_ip_of_request(request)
     if ip:
-        g = GeoIP(cache=CACHE_METHOD)
-        return g.country_name(ip)
+        return geoip.country_name(ip)
     return None
